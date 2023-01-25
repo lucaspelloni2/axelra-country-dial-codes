0d2de87eacd58e11475cb928dc7ee229ccc4df72
Lucas Pelloni <lucas_pelloni@hotmail.com>
2023-01-25 17:28:20 +0100

    data and svg library

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
 import { rawCountries } from "./raw-countries";
+import * as fs from "fs";
 
-rawCountries.map((c) => console.log("asf", c[0]));
+const toPrint = rawCountries.map((c) => {
+  const name = c[0] as string;
+  const flagXML = fs.readFileSync(
+    `src/flags/${name.toLowerCase().replace(/ /g, "-")}.svg`
+  );
+
+  return {
+    name,
+    iso2: c[2],
+    dial: c[3],
+    flag: flagXML.toString("utf-8"),
+  };
+});
+
+const string = toPrint.map((value, i) => {
+  const row = `{name: "${value.name}", iso2: "${value.iso2}", dial: ${value.dial}, flag: \`${value.flag}\`}`;
+  return i === toPrint.length - 1 ? `${row}\n` : `${row},\n`;
+});
+
+fs.writeFileSync(
+  "data.ts",
+  `export const countriesWithDial = [${string.join("")}];`
+);
